08c0c86d69babf148dfa63278cf49c7af911722d
Kobe Barrette <116814632+GitHerDone17@users.noreply.github.com>
2023-03-31 22:50:38 -0400

    01/17/2023 9:19am Implemented nameFilter

--- src/main/java/c/application/FilterProcessingElement.java
+++ src/main/java/c/application/FilterProcessingElement.java
@@ -3,7 +3,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package c.application;
-
+import java.io.File;
+import java.io.FileOutputStream;
+import com.laserfiche.repository.api.clients.impl.model.Entry;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -11,19 +14,83 @@ import java.util.List;
  * @author 98910
  */
   class FilterProcessingElement {
+      //Operator enum used for lengthFilter
+       public enum Operator{
+            EQ, NEQ, GT, GTE, LT, LTE
+        }
     public List<Entry> nameFilter(List<Entry> entries, String key) {
-        //implement
+        List<Entry> filteredNames = new ArrayList<>();
+        
+        if (entries instanceof RemoteEntry){
+        for (Entry entry: entries){
+            if(entry.getName().contains(key)){
+            filteredNames.add(entry);
+            }
+         }
+       }
+        else if (entries instanceof LocalEntry){
+        for (Entry entry: entries){
+            if(entry.getName().contains(key)){
+            filteredNames.add(entry);
+            }
+        }
+      }
+        return filteredNames;
     }
     
-    public List<Entry> lengthFilter(List<Entry> entries, long length, String operator) {
-        //implement
+    public static List<Entry> lengthFilter(List<Entry> entries, long length, Operator operator) {
+        List<Entry> filteredLen = new ArrayList<>();
+       
+        for (Entry entry : entries){
+            if(entry.getEntryType().toString().equals("Document")){
+             Long fileLen = entry.getElecDocumentSize();  
+             
+             switch (operator) {
+                 case EQ:
+                     if(fileLen == length){
+                         filteredLen.add(entry);
+                     }
+                     break;
+                 case NEQ: 
+                     if (fileLen != length){
+                         filteredLen.add(entry);
+                     }
+                     break;
+                 case GT: 
+                     if(fileLen > length) {
+                         filteredLen.add(entry);
+                     }
+                     break;
+                 case GTE: 
+                     if(fileLen >= length){
+                         filteredLen.add(entry);
+                     }
+                     break;
+                 case LTE: 
+                     if(fileLen <= length){
+                         filteredLen.add(entry);
+                     }
+                     break;
+                 case LT: 
+                     if(fileLen < length){
+                         filteredLen.add(entry);
+                     }
+                     break;
+                 default: 
+                     break;
+              }
+           }
+        }
+        return null;
     }
     
-    public List<Entry> contentFilter(List<Entry> entries, String key) {
+    public static List<Entry> contentFilter(List<Entry> entries, String key) {
         //implement
+        return null;
     }
     
-    public List<Entry> countFilter(List<Entry> entries, String key, int min) {
+    public static List<Entry> countFilter(List<Entry> entries, String key, int min) {
         //implement
+        return null;
     }
 }
