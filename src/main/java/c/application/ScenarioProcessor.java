96cf0db9ec30e546ee3acd3624aa5784fe0c6398
Mani <manirash94@gmail.com>
2023-04-02 23:52:23 -0400

    scenario parser complete

--- src/main/java/c/application/ScenarioProcessor.java
+++ src/main/java/c/application/ScenarioProcessor.java
@@ -4,29 +4,135 @@
  */
 package c.application;
 
-import com.nimbusds.jose.shaded.json.parser.JSONParser;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import kong.unirest.json.JSONException;
+import java.util.ArrayList;
+import java.util.List;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+
 
 public class ScenarioProcessor {
-    public static void main(String[] args) {
-        JSONParser parser = new JSONParser();
-        
-        try {     
-            Object obj = parser.parse(new FileReader(args[0]));
-            JSONArray jsonArray = (JSONArray) obj;
-            
-            JSONObject name = (JSONObject)jsonArray.get(0);
-            
+    public static void main(String[] args) throws FileNotFoundException, ParseException {
+////        public static void main(String[] args) {
+//        if (args.length == 0) {
+//            System.out.println("Please provide the JSON file path as the first command line argument.");
+//            System.exit(1);
+//        }
+
+//        String jsonFilePath = args[0];
+        String jsonFilePath = "C:\\Users\\98910\\Downloads\\Test Scenario.json";
+
+        try (FileReader reader = new FileReader(jsonFilePath)) {
+            JSONParser jsonParser = new JSONParser();
+            JSONObject scenario = (JSONObject) jsonParser.parse(reader);
+
+            System.out.println("name: " + scenario.get("name"));
+            JSONArray proc_els = (JSONArray) scenario.get("processing_elements");
+            for(Object p : proc_els) {
+                JSONObject proc_element = (JSONObject) p;
+                String type = (String)proc_element.get("type");
+                JSONArray inp_entrs = (JSONArray)proc_element.get("input_entries");
+                List<Entry> entries = new ArrayList<Entry>();
+                for(Object e : inp_entrs) {
+                    JSONObject entry = (JSONObject) e;
+                    if(entry.get("type").equals("local")){
+                        entries.add(new LocalEntry((String) entry.get("path")));
+                    } else if(entry.get("type").equals("remote")) {
+                        entries.add(new RemoteEntry((String)entry.get("repositoryId"), Integer.parseInt((String)entry.get("entryId"))));
+                    }
+                }
+                JSONArray params0 = (JSONArray)proc_element.get("parameters");
+                List<JSONObject> params = new ArrayList<JSONObject>();
+                for(int i =0; i<params0.size(); i++){
+                    params.add((JSONObject)params0.get(i));
+                }
+                switch (type){
+                    case "Name Filter":
+                        String Key = "";
+                        for(JSONObject q : params){
+                            if(q.get("name").equals("Key")){
+                                Key = (String)q.get("value");
+                            }
+                        }
+                        FilterProcessingElement.nameFilter(entries, Key);
+                        break;
+                    case "Length Filter":
+                        String Operator = "";
+                        long Length = 0;
+                        for(JSONObject q : params){
+                            if(q.get("name").equals("Length")){
+                                Length = Long.parseLong((String)q.get("value"));
+                            }
+                            if(q.get("name").equals("Operator")){
+                                Operator = (String)q.get("value");
+                            }
+                        }
+                        FilterProcessingElement.lengthFilter(entries, Length, Operator);
+                        break;
+                    case "Content Filter":
+                        String Key1 = "";
+                        for(JSONObject q : params){
+                            if(q.get("name").equals("Key")){
+                                Key1 = (String)q.get("value");
+                            }
+                        }
+                        FilterProcessingElement.contentFilter(entries, Key1);
+                        break;
+                    case "Count Filter":
+                        String Key2 = "";
+                        int min = 0;
+                        for(JSONObject q : params){
+                            if(q.get("name").equals("Key")){
+                                Key2 = (String)q.get("value");
+                            }
+                            if(q.get("name").equals("Min")){
+                                min = Integer.parseInt((String)q.get("value"));
+                            }
+                        }
+                        FilterProcessingElement.countFilter(entries, Key2, min);
+                        break;
+                    case "Split":
+                        int Lines = 0;
+                        for(JSONObject q : params){
+                            if(q.get("name").equals("Lines")){
+                                Lines = Integer.parseInt((String)q.get("value"));
+                            }
+                        }
+                        SplitProcessingElement.process(entries, Lines);
+
+                        break;
+                    case "List":
+                        int max = 0;
+                        for(JSONObject q : params){
+                            if(q.get("name").equals("Max")){
+                                max = Integer.parseInt((String)q.get("value"));
+                            }
+                        }
+                        ListProcessingElement.process(entries, max);
+                        break;
+                    case "Rename":
+                        String Suffix = "";
+                        for(JSONObject q : params){
+                            if(q.get("name").equals("Suffix")){
+                                Suffix = (String)q.get("value");
+                            }
+                        }
+                        RenameProcessingElement.process(entries, Suffix);
+                        break;
+                    case default:
+                        PrintProcessingElement.process(entries);
+                        break;
+                }
+            }
+            System.out.println(proc_els.get(1).getClass().getName());
+        } catch (IOException | ParseException e) {
+            System.out.println("Error parsing JSON file:");
+            e.printStackTrace();
         }
     }
 }
